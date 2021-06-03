8b39ea8ea28bf0fe73a3bab10bb96d346fc52ddb
sakasegawajunki <j.sakasegawa18@gmail.com>
2021-06-03 05:54:40 +0000

    [s]

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,12 @@ Rails.application.routes.draw do
   root to: "homes#top"
   get "/home/about" =>"homes#about"
   devise_for :users
-  resources :users,only: [:show,:edit,:update, :index,]
+  resources :users,only: [:show,:edit,:update, :index,] do
+   resource :relationships, only: [:create, :destroy]
+   get "followings" => "relationships#followings", as: "followings"
+   get "followers" => "relationships#followers", as: "followers"
+ end
+ 
   resources :books,only: [:new, :create, :index, :show, :destroy, :edit, :update] do
     resource :favorites, only: [:create, :destroy]
     resources :book_comments,only: [:create, :destroy]
